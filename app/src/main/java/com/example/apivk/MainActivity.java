85d1746ed964e47d130ca77655082091eddbb52c
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2020-07-18 04:42:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/apivk/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.apivk;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    public String[] scope = new String[]{VKScope.FRIENDS};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        VKSdk.login(this, scope);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                VKRequest vkRequest = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "firstName,lastName"));
+                vkRequest.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+
+                        VKList list = (VKList) response.parsedModel;
+
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, list);
+                        listView.setAdapter(arrayAdapter);
+                    }
+                });
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        }))
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+}
